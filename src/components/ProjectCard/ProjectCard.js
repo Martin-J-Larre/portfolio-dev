3b9667c9efd3b673f33c729e8e8d7d1c518f0745
Martin-J-Larre <martinjlarre@gmail.com>
2023-01-08 00:59:42 -0500

    #10 project images front updated

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,29 +1,28 @@
-import { Col, Row } from 'react-bootstrap';
 import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
 import './projectCard.css'
 
-export const ProjectCard = ({imgUrl, title, description, gitHubUrl, demoUrl}) => {
-  return (
-          <div className="proj-imgbx">
-              <img src={imgUrl} />
-              <div className="proj-txtx">
-                  <h4>{title}</h4>
-                  <span>{description}</span>
-                  <div className='proj-icons'>
-                            <a
-                                href={ gitHubUrl } 
-                                target="_blank"
-                            >
-                                <FaGithub />
-                            </a>
-                            <a
-                                href={ demoUrl } 
-                                target="_blank"
-                            >
-                                <FaExternalLinkAlt />
-                            </a>
-                  </div>
-              </div>
-          </div>
-  );
+export const ProjectCard = ({ imgUrl, title, description, gitHubUrl, demoUrl }) => {
+    return (
+        <div className="proj-imgbx">
+            <img src={imgUrl} />
+            <div className="proj-txtx">
+                <h4>{title}</h4>
+                <span>{description}</span>
+                <div className='proj-icons'>
+                    <a
+                        href={gitHubUrl}
+                        target="_blank"
+                    >
+                        <FaGithub />
+                    </a>
+                    <a
+                        href={demoUrl}
+                        target="_blank"
+                    >
+                        <FaExternalLinkAlt />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/helpers/projectsListFront.js
+++ src/helpers/projectsListFront.js
@@ -1,217 +1,76 @@
-import projImg1 from '../assets/img/p-1.png';
-import projImg2 from '../assets/img/p-2.png';
-import projImg3 from '../assets/img/p-3.png';
-import projImg4 from '../assets/img/p-4.png';
+import oishiiChefJs from '../assets/img/oishii-chef-js.png';
+import quoteGenerator from '../assets/img/quote-generator.png';
+import clipboard from '../assets/img/clipboard.png';
+import twClone from '../assets/img/tw-clone.png';
+import dropdown from '../assets/img/dropdown.png';
+import infinityScroll from '../assets/img/infinity-scroll.png';
+import pictureInPicture from '../assets/img/picture-in-picture.png';
+import jokeTeller from '../assets/img/joke-teller.png';
+import darkMode from '../assets/img/dark-mode.png';
+
 
 export const projectsListFront = [
   {
-    title: "Landing page",
+    title: "Light/Dark Mode",
+    description: "HTML, CSS, Javascript",
+    imgUrl: darkMode,
+    gitHubUrl:"https://github.com/Martin-J-Larre/dark-mode",
+    demoUrl:"https://martin-j-larre.github.io/dark-mode/"
+  },
+  {
+    title: "Joke Teller",
+    description: "HTML, CSS, Javascript",
+    imgUrl: jokeTeller,
+    gitHubUrl:"https://github.com/Martin-J-Larre/joke-teller",
+    demoUrl:"https://martin-j-larre.github.io/joke-teller/"
+  },
+  {
+    title: "Picture in Picture",
+    description: "HTML, CSS, Javascript",
+    imgUrl: pictureInPicture,
+    gitHubUrl:"https://martin-j-larre.github.io/picture-in-picture/",
+    demoUrl:"https://martin-j-larre.github.io/infinity-scroll/"
+  },
+  {
+    title: "Infinity Scroll",
+    description: "HTML, CSS, Javascript",
+    imgUrl: infinityScroll,
+    gitHubUrl:"https://github.com/Martin-J-Larre/infinity-scroll",
+    demoUrl:"https://martin-j-larre.github.io/infinity-scroll/"
+  },
+  {
+    title: "Dropdown Nav",
+    description: "HTML, CSS, Javascript",
+    imgUrl: dropdown,
+    gitHubUrl:"https://github.com/Martin-J-Larre/intro-section-with-dropdown-navigation",
+    demoUrl:"https://martin-j-larre.github.io/intro-section-with-dropdown-navigation/"
+  },
+  {
+    title: "Tw Clone",
+    description: "React, MUI, Firebase",
+    imgUrl: twClone,
+    gitHubUrl:"https://github.com/Martin-J-Larre/tw-clone",
+    demoUrl:"https://martin-j-larre.github.io/tw-clone/"
+  },
+  {
+    title: "Clipboad Landing Page",
+    description: "Html, css",
+    imgUrl: clipboard,
+    gitHubUrl:"https://github.com/Martin-J-Larre/Clipboad-Landing-Page",
+    demoUrl:"https://martin-j-larre.github.io/Clipboad-Landing-Page/"
+  },
+  {
+    title: "Quote Generator",
     description: "Html, css, Javascript",
-    imgUrl: projImg1,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
+    imgUrl: quoteGenerator,
+    gitHubUrl:"https://github.com/Martin-J-Larre/quote-generator",
+    demoUrl:"https://martin-j-larre.github.io/quote-generator/"
   },
   {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg2,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg3,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg4,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg4,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg4,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg4,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg4,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg4,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg4,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg4,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg4,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg4,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg4,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg4,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Landing page",
+    title: "Ecommerce UI",
     description: "Html, css, Javascript",
-    imgUrl: projImg1,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
+    imgUrl: oishiiChefJs,
+    gitHubUrl:"https://github.com/Martin-J-Larre/ProyectoFinal-JS-CoderHouse-Larre-",
+    demoUrl:"https://martin-j-larre.github.io/ProyectoFinal-JS-CoderHouse-Larre-/"
   },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg2,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg3,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Landing page",
-    description: "Html, css, Javascript",
-    imgUrl: projImg1,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg2,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg3,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Landing page",
-    description: "Html, css, Javascript",
-    imgUrl: projImg1,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg2,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg3,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Landing page",
-    description: "Html, css, Javascript",
-    imgUrl: projImg1,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg2,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg3,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Landing page",
-    description: "Html, css, Javascript",
-    imgUrl: projImg1,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg2,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  },
-  {
-    title: "Business Startup",
-    description: "Design & Development",
-    imgUrl: projImg3,
-    gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-    demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  }
 ];
