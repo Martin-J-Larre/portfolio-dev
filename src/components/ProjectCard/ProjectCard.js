d34743ff81b2f2b35bc0b93b7470dee70f110360
Martin-J-Larre <martinjlarre@gmail.com>
2023-04-01 21:48:41 -0400

    #26 mern project updated

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,32 +1,30 @@
-import { FaGithub, FaExternalLinkAlt, FaWindowClose } from 'react-icons/fa';
-import './projectCard.css'
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
+import "./projectCard.css";
 
-export const ProjectCard = ({ imgUrl, title, description, gitHubUrl, demoUrl }) => {
-    const widthScreen = window.innerWidth;
+export const ProjectCard = ({
+  imgUrl,
+  title,
+  description,
+  gitHubUrl,
+  demoUrl,
+}) => {
+  const widthScreen = window.innerWidth;
 
-
-    
-    return (
-        <div className="proj-imgbx">
-            <img src={imgUrl} />
-            <div className="proj-txtx">
-                <h4>{title}</h4>
-                <span>{description}</span>
-                <div className='proj-icons'>
-                    <a
-                        href={gitHubUrl}
-                        target="_blank"
-                    >
-                        <FaGithub />
-                    </a>
-                    <a
-                        href={demoUrl}
-                        target="_blank"
-                    >
-                        <FaExternalLinkAlt />
-                    </a>
-                </div>
-            </div>
+  return (
+    <div className="proj-imgbx">
+      <img src={imgUrl} />
+      <div className="proj-txtx">
+        <h4>{title}</h4>
+        <span>{description}</span>
+        <div className="proj-icons">
+          <a href={gitHubUrl} target="_blank">
+            <FaGithub />
+          </a>
+          <a href={demoUrl} target="_blank">
+            <FaExternalLinkAlt />
+          </a>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
--- src/helpers/projectsListBack.js
+++ src/helpers/projectsListBack.js
@@ -1,13 +1,27 @@
-
-// import projImg3 from '../assets/img/p-3.png';
-
+import apiBlogNode from "../assets/img/blog-screen.png";
+import socialMediaTW from "../assets/img/social-media-screen.png";
+import musicApp from "../assets/img/music-app-screen.png";
 
 export const projectsListBack = [
-  // {
-  //   title: "Business Startup",
-  //   description: "Design & Development",
-  //   imgUrl: projImg3,
-  //   gitHubUrl:"https://github.com/Martin-J-Larre/test-2-heroku",
-  //   demoUrl:"https://github.com/Martin-J-Larre/test-2-heroku"
-  // }
+  {
+    title: "API Blog",
+    description: "NodeJS, Express, MongoDB, MVC",
+    imgUrl: apiBlogNode,
+    gitHubUrl: "https://github.com/Martin-J-Larre/api-blog-node",
+    demoUrl: "https://api-blog-node.onrender.com/",
+  },
+  {
+    title: "API Social Media",
+    description: "NodeJS, Express, MongoDB, MVC",
+    imgUrl: socialMediaTW,
+    gitHubUrl: "https://github.com/Martin-J-Larre/api-social-media-node",
+    demoUrl: "https://api-social-media-node.onrender.com/",
+  },
+  {
+    title: "API Music App",
+    description: "NodeJS, Express, MongoDB, MVC",
+    imgUrl: musicApp,
+    gitHubUrl: "https://github.com/Martin-J-Larre/music-app-node",
+    demoUrl: "https://music-app-node.onrender.com",
+  },
 ];
